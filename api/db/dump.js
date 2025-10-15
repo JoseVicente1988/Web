1ecd7fd35446d6a0536c278287a4b94e830c2432
Jose Vicente Vicedo Silla <josevicentevicedosilla@gmail.com>
2025-10-15 22:51:53 +0200

    Change data object to sample in db dump response

--- api/db/dump.js
+++ api/db/dump.js
@@ -1,15 +1,14 @@
-// api/db/dump.js
 const { getDB, listTables, runSelect } = require("../_db");
 
 module.exports = async (req, res) => {
   try {
     const db = await getDB();
     const tables = listTables(db);
-    const data = {};
+    const sample = {};
     for (const t of tables) {
-      data[t] = runSelect(db, `SELECT * FROM ${t} LIMIT 10;`);
+      sample[t] = runSelect(db, `SELECT * FROM ${t} LIMIT 10;`);
     }
-    res.status(200).json({ ok: true, tables, sample: data });
+    res.status(200).json({ ok: true, tables, sample });
   } catch (e) {
     res.status(500).json({ ok: false, error: e.message || String(e) });
   }
